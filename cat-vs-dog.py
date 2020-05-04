f112f4db89048a6d531317f6728aedb134943f5b
Aung_Thura <55635378+atr00025@users.noreply.github.com>
2020-05-04 16:59:24 +0630

    Add files via upload

---
+++ cat-vs-dog.py
@@ -0,0 +1,122 @@
+import os
+import zipfile
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+local_weights_file = 'C:/ML things/Image_classification_part3/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+pre_trained_model = InceptionV3(
+    input_shape=(150, 150, 3), include_top=False, weights=None)
+pre_trained_model.load_weights(local_weights_file)
+
+for layer in pre_trained_model.layers:
+    layer.trainable = False
+
+last_layer = pre_trained_model.get_layer('mixed7')
+print('last layer output shape: ', last_layer.output_shape)
+last_output = last_layer.output
+
+x = layers.Flatten()(last_output)
+
+x = layers.Dense(1024, activation='relu')(x)
+
+x = layers.Dropout(0.2)(x)
+
+x = layers.Dense(1, activation='sigmoid')(x)
+
+model = Model(pre_trained_model.input, x)
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.0001),
+              metrics=['acc'])
+
+base_dir = 'C:/ML things/Image_classification_part2/cats_and_dogs_filtered'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+#Dir with training cat data
+train_cats_dir = os.path.join(train_dir, 'cats')
+
+#Dir with training dog 
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+#Dir with validaton cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+ 
+#Dir with validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+train_cat_fnames = os.listdir(train_cats_dir)
+train_dog_fnames = os.listdir(train_dogs_dir)
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+validation_generator = val_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=2,
+    validation_data=validation_generator,
+    validation_steps=50,
+    verbose=2)
+
+unfreeze = False
+for layer in pre_trained_model.layers:
+    if unfreeze:
+        layer.trainable = True
+    if layer.name == 'mixed6':
+         unfreeze = True
+
+model.compile(loss='binary_crossentropy',
+                optimizer=SGD(lr=0.0001,
+                            momentum=0.9),
+                metrics=['acc'])
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=50,
+    validation_data=validation_generator,
+    validation_steps=50,
+    verbose=2)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc)
+plt.plot(epochs, val_acc)
+plt.title('Trianing and validation accurency.')
+plt.figure()
+
+plt.plot(epochs, loss)
+plt.plot(epochs, val_loss)
+plt.title('Trianing and validation loss.')
